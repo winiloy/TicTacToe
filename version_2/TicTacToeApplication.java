f6f32c83a5409ae4c0b1713b77d6790129c021f5
javed-hossain <javedhossin.exe@gmail.com>
2020-11-07 18:25:01 +0600

    Created two folders, moved previous files to version_2

---
+++ version_2/TicTacToeApplication.java
@@ -0,0 +1,15 @@
+/**
+ * Contains the main method. Creates a ConsoleRunner and then calls its
+ * mainLoop method.
+ *
+ * @author INSERT YOUR NAME(S) HERE
+ */
+
+public class TicTacToeApplication {
+
+    public static void main(String[] args) {
+        /*
+         * TBD
+         */
+    }
+}
